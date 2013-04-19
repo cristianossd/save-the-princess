e76ea17a84a8cc2f8c37cbf28df3b358075f89d3
Cristiano Santos <cristianossd@gmail.com>
2013-04-18 22:29:40 -0300

    Add attack() and defense() in characters

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,4 +2,24 @@
 class Character
 	# Character profile attributes
 	attr_accessor :name
+
+	# Character's actions
+
+	def self.attack()
+		puts "What the strenght?"
+		strenght = gets.to_i
+
+		attk = strenght.times.collect do
+			rand(6)+1
+		end.injected(:+)
+	end
+
+	def self.defense()
+		puts "What the armor?"
+		armor = gets.to_i
+
+		defs = armor.times.collect do
+			rand(6)+1
+		end.injected(:+)
+	end
 end
